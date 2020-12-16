ac59a278c67a537e1c46bb4e46ae7dda571fa790
Thanwar <thawardas25@gmail.com>
2020-12-16 22:24:24 +0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function App() {
+  return (
+    <div>
+      Shoe Store
+    </div>
+  );
+}
+
+export default App;
